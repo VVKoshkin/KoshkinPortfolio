8666a17dbecc3af1d714da268bca7c9789059435
VV.Koshkin <VV.Koshkin@e-soft.ru>
2023-01-20 11:43:32 +0300

    Started making dark/light themes

--- js/main.js
+++ js/main.js
@@ -2,4 +2,15 @@ $(() => {
     $('.navbar-hamburger').click(() => {
         $('.navbar-elements').toggle();
     });
+    $('#color-mode-switcher').click(function (e) {
+        e.preventDefault();
+        setMode('dark');
+    });
 });
+
+const setMode = (mode) => {
+    const modeName = mode + 'Mode';
+    less.modifyVars({ 
+        navbarBgc: `@navbarBgcModes[${modeName}]`
+    });
+}
