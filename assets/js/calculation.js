de1e043da54df943a2db8ed4f9c8642d72576701
VV.Koshkin <VV.Koshkin@e-soft.ru>
2023-02-02 18:51:51 +0300

    processing calculation form

--- assets/js/calculation.js
+++ assets/js/calculation.js
@@ -1,5 +1,5 @@
 const blocksAndTheirListeners = {
-    'calc-get-type':() => {
+    'calc-get-type': () => {
         $('#otherTypeDesc').parent('label').hide();
         $('#calc-site-type').change((e) => {
             const optionValue = e.target.selectedOptions[0].value;
@@ -9,21 +9,125 @@ const blocksAndTheirListeners = {
                 $('#otherTypeDesc').parent('label').hide();
             if (optionValue === "Landing") {
                 $('#sitePageAmount').val(1);
-                $("input").prop('disabled', true);
+                $('#sitePageAmount').prop('disabled', true);
             } else {
-                $("input").prop('disabled', false);
+                $('#sitePageAmount').prop('disabled', false);
             }
         });
         $('#calc-next-btn').click((e) => {
-            renderBlock('calc-has-design', true);
+            if (!checkFilled(['#calc-site-type', '#sitePageAmount'])) return;
+            addToResults('site-type', $('#calc-site-type').val());
+            addToResults('site-type-desc', $('#otherTypeDesc').val());
+            addToResults('site-total-pages', $('#sitePageAmount').val())
+            renderBlock('calc-has-design');
         })
     },
-    'calc-has-design':() => {
-        // TODO
+    'calc-has-design': () => {
+        $('#calc-next-btn').hide();
+        $('#calc-has-design .btn').show();
+        $('#calc-has-design .btn').click((e) => {
+            const res = Boolean($(e.target).data('btn-result'));
+            addToResults('site-has-design', res);
+            if (res)
+                renderBlock('calc-get-pages');
+            else
+                renderBlock('calc-need-adaptive');
+
+        })
+    },
+    'calc-get-pages': () => {
+        const pagesAmount = getFromResults('site-total-pages');
+        if (pagesAmount == 1)
+            $('.calc-block-pages-grid').addClass('solo-page');
+        const blockPageTemplate = $('.calc-block-page');
+        $('.calc-block-page').remove();
+        for (let i = 1; i <= pagesAmount; i++) {
+            const clone = $(blockPageTemplate).clone().data('page-num', i);
+            $(clone).children('.typeset-big').text(`Страница ${i}`);
+            $('.calc-block-pages-grid').append(clone);
+        }
+        $('label[name="simpleHasAnim"]').click((e) => {
+            let target = e.target;
+            if (!$(target).is('label[name="simpleHasAnim"]'))
+                target = $(e.target).parent('label[name="simpleHasAnim"]');
+            $(target).siblings('label[name="simpleSectionsAnimAmount"]').children('input[type="number"]').prop("disabled", !$(e.target).is(':checked'));
+        });
+        $('label[name="hardHasAnim"]').click((e) => {
+            let target = e.target;
+            if (!$(target).is('label[name="hardHasAnim"]'))
+                target = $(e.target).parent('label[name="hardHasAnim"]');
+            $(target).siblings('label[name="hardSectionsAnimAmount"]').children('input[type="number"]').prop("disabled", !$(e.target).is(':checked'));
+        });
+        $('#calc-next-btn').click((e) => {
+            if (!checkFilled(['label[name="simpleSectionsAmount"] input', 'label[name="hardSectionsAmount"] input'])) return;
+            const results = {};
+            $('.calc-block-page').each((i, elem) => {
+                const elemResult = {
+                    'simpleSectionsAmount': $(elem).find('label[name="simpleSectionsAmount"] input[type="number"]').val(),
+                    'hardSectionsAmount': $(elem).find('label[name="hardSectionsAmount"] input[type="number"]').val(),
+                    'simpleHasAnim': $(elem).find('label[name="simpleHasAnim"] input[type="checkbox"]').is(':checked'),
+                    'simpleSectionsAnimAmount': $(elem).find('label[name="simpleSectionsAnimAmount"] input[type="number"]').val(),
+                    'hardHasAnim': $(elem).find('label[name="hardHasAnim"] input[type="checkbox"]').is(':checked'),
+                    'hardSectionsAnimAmount': $(elem).find('label[name="hardSectionsAnimAmount"] input[type="number"]').val()
+                }
+                results[`page-${i + 1}`] = elemResult;
+            });
+            addToResults('site-pages', results);
+            console.log(results);
+            renderBlock('calc-design-has-adaptive');
+        })
+    },
+    'calc-design-has-adaptive': () => {
+        $('#calc-next-btn').hide();
+        $('#calc-design-has-adaptive .btn').show();
+        $('#calc-design-has-adaptive .btn').click((e) => {
+            const res = Boolean($(e.target).data('btn-result'));
+            addToResults('site-design-has-adaptive', res);
+            renderBlock('calc-get-offcanvas');
+        })
+    },
+    'calc-need-adaptive': () => {
+        $('#calc-next-btn').hide();
+        $('#calc-need-adaptive .btn').show();
+        $('#calc-need-adaptive .btn').click((e) => {
+            const res = Boolean($(e.target).data('btn-result'));
+            addToResults('site-need-adaptive', res);
+            renderBlock('calc-get-offcanvas');
+        })
+    },
+    'calc-get-offcanvas': () => {
+        $('#calc-next-btn').hide();
+        $('#calc-get-offcanvas .btn').show();
+        $('#calc-get-offcanvas .btn').click((e) => {
+            const res = Boolean($(e.target).data('btn-result'));
+            addToResults('site-need-offcanvas', res);
+            renderBlock('calc-get-has-backend');
+        })
+    },
+    'calc-get-has-backend': () => {
+        const siteType = getFromResults('site-type');
+        if (['personalBlog', 'portal', 'socnet', 'shop'].indexOf(siteType) > -1) {
+            addToResults('site-need-backend', true);
+            renderBlock('calc-get-backend-type', true);
+            return;
+        }
+        $('#calc-next-btn').hide();
+        $('#calc-get-has-backend .btn').show();
+        $('#calc-get-has-backend .btn').click((e) => {
+            const res = Boolean($(e.target).data('btn-result'));
+            addToResults('site-need-backend', res);
+            if (res)
+                renderBlock('calc-get-backend-type', true);
+            else
+                setFinal();
+        })
+    },
+    'calc-get-backend-type': () => {
+        return;
     }
 }
 
-$(()=> {
+$(() => {
     $('.calc-block').hide();
     $('.calc-btn').hide();
     renderStart();
@@ -40,11 +144,68 @@ const renderStart = () => {
 
 const renderBlock = (blockId, isFinal = false) => {
     $('.calc-btn').hide();
+    $('#calc-next-btn').off();
     $('.calc-block').hide();
     $(`#${blockId}`).show();
     if (isFinal)
         $('#calc-finish-btn').show();
     else
         $('#calc-next-btn').show();
-    blocksAndTheirListeners[blockId]();    
+    blocksAndTheirListeners[blockId]();
+    if (isFinal) {
+        $('#calc-finish-btn').click(setFinal);
+    }
+}
+
+const checkFilled = (elemSelectorsCol) => {
+    let res = true;
+    elemSelectorsCol.forEach((elemSelector) => {
+        $(elemSelector).each((i, elem) => {
+            if ($(elem).val() === 'none' || $(elem).val() === '') {
+                showUnFilled(elem);
+                res = false;
+            }
+        })
+    })
+    return res;
+}
+
+const addToResults = (fieldName, fieldValue) => {
+    let resultsJSON = JSON.parse($('#results-input').val());
+    resultsJSON[fieldName] = fieldValue;
+    $('#results-input').val(JSON.stringify(resultsJSON));
+}
+
+const getFromResults = (fieldName) => {
+    const resultsJSON = JSON.parse($('#results-input').val());
+    return resultsJSON[fieldName];
+}
+
+const setFinal = () => {
+    if ($('#calc-backend-type').is(':visible') && !checkFilled(['#calc-backend-type'])) return;
+    addToResults('site-backend-type', $('#calc-backend-type').val());
+    const resultsJSON = JSON.parse($('#results-input').val());
+
+    const resultPrice = (totalPages, totalSimpleScreens, totalHardScreens, totalAnimations, kBackend, hasDesign, hasResponsiveDesign, hasOffcanvas) => {
+        /* смысл формулы:
+            !hasDesign * (totalPages * 2000 * (4 - hasResponsiveDesign))
+            если дизайна нет, его надо сначала сделать
+            если вдруг клиенту не надо адаптивный дизайн, а только под комп, то скидка на этот пункт 2000 рублей, иначе по 8000 за страницу + адаптив
+            3000 + 1000 * (totalPages - 1)
+            далее 3000 базово за 1 свёрстанную страницу. 1 свёрстанная страница это хэдер, футер и 1 секция
+            но поскольку хэдер и футер как правило повторяются, то 3000 только за 1 страницу, а дальше по 1000
+            hasDesign * ((totalSimpleScreens + 2 * totalHardScreens - 3 - (totalPages - 1) - !hasResponsiveDesign * 0.3 * (totalSimpleScreens + totalHardScreens)) * 1000 + totalAnimations * 500)
+            следующее - если есть дизайн уже, тогда только вёрстка. каждый дополнительный экран 1000 рублей. сложные по двойному тарифу.
+            каждая анимация ещё 500 рублей. за отсутствие адаптива небольшое понижение ценника
+            если же дизайна нет, то 
+            !hasDesign * 1500 * totalPages * (4 - hasResponsiveDesign)
+            ориентировочно по 6000 рублей за страницу поверху тех 3 тысяч базовых, по 4500 если без адаптивного дизайна 
+            оффканвасы ориентрировочно 2 тысячи, ибо там немного посложнее, там ведь и JS надо настроить, и слушателей навешать и всё такое
+            последнее - это бэкенд, тут всё просто, по 5000 за посадку одной страницы базово, а дальше в зависимости от выбранного бэка всё или сложнее,
+            или может легче даже, или так же. kBackend - коэффициент сложности посадки
+        */
+        return !hasDesign * (totalPages * 2000 * (4 - hasResponsiveDesign)) + 3000 + 1000 * (totalPages - 1) + hasDesign * ((totalSimpleScreens + 2 * totalHardScreens - 3 - (totalPages - 1) - !hasResponsiveDesign * 0.3 * (totalSimpleScreens + totalHardScreens)) * 1000 + totalAnimations * 500) + !hasDesign * 1500 * totalPages * (4 - hasResponsiveDesign) + 2000 * hasOffcanvas + 5000 * totalPages * kBackend;
+    }
+    
+
 }
--- page-calculation.php
+++ page-calculation.php
@@ -1,12 +1,14 @@
 <?php get_header('default'); ?>
+<input type="hidden" name="results-input" id="results-input" value = "{}">
 <section class="calc-section">
     <div class="container">
         <div class="calc-block calc-start" id="calc-start">
             <h4 class="headline">Страница расчёта</h4>
-            <p class="typeset">На этой странице вы действительно сможете посчитать цену сайта, и в конце опроса вам реально покажут стоимость и даже смету. Но она лишь примерная. Окончательную стоимость можно будет узнать только после нашего с вами общения и составления технического задания. Поэтому кнопка для направления заявки тоже будет ожидать вас в конце. Расчёт будет более точен, если у вас уже есть дизайн-макет. Нажмите на кнопку ниже и приготовьтесь пройти небольшой опрос</p>
+            <p class="typeset">На этой странице вы действительно сможете посчитать цену сайта, и в конце опроса вам реально покажут стоимость. Но она лишь примерная. Окончательную стоимость можно будет узнать только после нашего с вами общения и составления технического задания. К тому же, короткий опрос просто не может предусмотреть всё и вся, т.к. разные типы сайтов могут иметь совершенно разный необходимый набор программных решений. Поэтому кнопка для направления заявки тоже будет ожидать вас в конце. Расчёт будет более точен, если у вас уже есть дизайн-макет. Нажмите на кнопку ниже и приготовьтесь пройти небольшой опрос</p>
         </div>
         <div class="calc-block get-type" id="calc-get-type">
             <h4 class="headline">Какого типа сайт и сколько на нём разных страниц?</h4>
+            <p class="typeset">Интересует именно количество разных по структуре страниц</p>
             <div class="inner">
                 <select name="site-type" id="calc-site-type">
                     <option value="none">--Выбрать--</option>
@@ -24,61 +26,70 @@
         </div>
         <div class="calc-block get-has-design" id="calc-has-design">
             <h4 class="headline">У вас уже есть дизайн-макет?</h4>
+            <p class="typeset">Макет делает дизайнер, как правило, в таких программах как Figma или Photoshop</p>
             <div class="btns">
-                <button class="btn calc-btn">Да</button>
-                <button class="btn calc-btn">Нет</button>
+                <button class="btn calc-btn" data-btn-result="true">Да</button>
+                <button class="btn calc-btn" data-btn-result="false">Нет</button>
             </div>
         </div>
-        <div class="calc-block get-pages">
+        <div class="calc-block get-pages" id="calc-get-pages">
             <h4 class="headline">Укажите параметры для страниц в макете</h4>
+            <p class="typeset">Чем точнее будет указано, тем точнее будет результат расчёта</p>
             <div class="calc-block-pages-grid">
-                <?php for($i = 1; $i<=2; $i++) { ?>
                 <div class="calc-block-page">
-                    <p class="typeset-big">Страница <?php echo $i; ?></p>
-                    <label class="typeset">Сколько экранов страницы вы бы оценили как простые:<input type="number" name="simpleSectionsAmount" id="simpleSectionsAmount"></label>
-                    <label class="typeset">Сколько экранов страницы вы бы оценили как сложные:<input type="number" name="simpleSectionsAmount" id="simpleSectionsAmount"></label>
-                    <label class="typeset">Будут ли анимации на простых страницах<input type="checkbox" name="hasAnim" id="hasAnim"></label>
-                    <label class="typeset">Сколько всего элементов простых экранов<br> будут подвергнуты анимации:<input type="number" name="simpleSectionsAnimAmount" id="simpleSectionsAnimAmount"></label>
-                    <label class="typeset">Будут ли анимации на сложных страницах<input type="checkbox" name="hasAnim" id="hasAnim"></label>
-                    <label class="typeset">Сколько всего элементов сложных экранов<br> будут подвергнуты анимации:<input type="number" name="simpleSectionsAnimAmount" id="simpleSectionsAnimAmount"></label>
+                    <p class="typeset-big">Страница ...</p>
+                    <label class="typeset" name="simpleSectionsAmount">Сколько экранов страницы вы бы оценили как простые:<input type="number"></label>
+                    <label class="typeset" name="hardSectionsAmount">Сколько экранов страницы вы бы оценили как сложные:<input type="number"></label>
+                    <label class="typeset" name="simpleHasAnim">Будут ли анимации на простых страницах<input type="checkbox"></label>
+                    <label class="typeset" name="simpleSectionsAnimAmount">Сколько всего элементов простых экранов<br> будут подвергнуты анимации:<input disabled type="number"></label>
+                    <label class="typeset" name="hardHasAnim">Будут ли анимации на сложных страницах<input type="checkbox"></label>
+                    <label class="typeset" name="hardSectionsAnimAmount">Сколько всего элементов сложных экранов<br> будут подвергнуты анимации:<input disabled type="number"></label>
                 </div>
-                <?php } ?>
             </div>
         </div>
-        <div class="calc-block get-has-adaptive" id="calc-has-adaptive">
+        <div class="calc-block get-has-adaptive" id="calc-design-has-adaptive">
+            <h4 class="headline">Есть ли в макете адаптивный дизайн?</h4>
+            <p class="typeset">Адаптивный дизайн - это дизайн одной и той же страницы для различных разрешений экранов (телефон, планшет, монитор)</p>
+            <div class="btns">
+                <button class="btn calc-btn" data-btn-result="true">Да</button>
+                <button class="btn calc-btn" data-btn-result="false">Нет</button>
+            </div>
+        </div>
+        <div class="calc-block get-has-adaptive" id="calc-need-adaptive">
             <h4 class="headline">Нужен ли будет адаптивный дизайн?</h4>
+            <p class="typeset">Адаптивный дизайн - это дизайн одной и той же страницы для различных разрешений экранов (телефон, планшет, монитор)</p>
             <div class="btns">
-                <button class="btn calc-btn">Да</button>
-                <button class="btn calc-btn">Нет</button>
+                <button class="btn calc-btn" data-btn-result="true">Да</button>
+                <button class="btn calc-btn" data-btn-result="false">Нет</button>
             </div>
         </div>
-        <div class="calc-block get-offcanvas">
-            <h4 class="headline">Должны ли быть на сайте модальные окна, перекрывающие собой весь контент при определённых условиях?</h4>
+        <div class="calc-block get-offcanvas" id="calc-get-offcanvas">
+            <h4 class="headline">Должны ли быть на сайте модальные окна, перекрывающие собой контент при определённых условиях?</h4>
+            <p class="typeset">Например, форма обратной связи, всплывающие окошки, чат с оператором</p>
             <div class="btns">
-                <button class="btn calc-btn">Да</button>
-                <button class="btn calc-btn">Нет</button>
-            </div>
-            <div class="inner">
-            <label class="typeset">Сколько таких окон будет:<input type="number" name="simpleSectionsAnimAmount" id="simpleSectionsAnimAmount"></label>
+                <button class="btn calc-btn" data-btn-result="true">Да</button>
+                <button class="btn calc-btn" data-btn-result="false">Нет</button>
             </div>
         </div>
-        <div class="calc-block get-has-backend">
+        <div class="calc-block get-has-backend" id="calc-get-has-backend">
             <h4 class="headline">Нужен ли сайту бэкенд или админпанель?</h4>
+            <p class="typeset">Изменять содержимое сайта без наличия бэкенда может только знакомый с HTML разметкой человек</p>
             <div class="btns">
-                <button class="btn calc-btn">Да</button>
-                <button class="btn calc-btn">Нет</button>
+                <button class="btn calc-btn" data-btn-result="true">Да</button>
+                <button class="btn calc-btn" data-btn-result="false">Нет</button>
             </div>
         </div>
-        <div class="calc-block get-backend-type">
-            <h4 class="headline">Что будет использоваться для этой цели?</h4>
+        <div class="calc-block get-backend-type" id="calc-get-backend-type">
+            <h4 class="headline">Какой будет бэкенд?</h4>
+            <p class="typeset">Бэкенд - это то, что наполняет контентом сайт, забирая и размещая у себя данные из управляемой пользователем базы данных</p>
             <div class="inner">
-                <select name="" id="">
+                <select name="" id="calc-backend-type">
                     <option value="none">--Выбрать--</option>
-                    <option value="WP">CMS WordPress</option>
-                    <option value="OC">CMS OpenCart</option>
+                    <option value="WordPress">CMS WordPress</option>
+                    <option value="OpenCart">CMS OpenCart</option>
                     <option value="Joomla">CMS Joomla</option>
-                    <option value="flask">Собственный движок на Python Flask</option>
-                    <option value="spring">Собственный движок на Java Spring</option>
+                    <option value="Flask">Собственный движок на Python Flask</option>
+                    <option value="Spring">Собственный движок на Java Spring Boot</option>
                     <option value="noidea">Нужна консультация/Мне без разницы</option>
                 </select>
             </div>
@@ -86,7 +97,7 @@
         <button class="btn calc-btn" id="calc-start-btn">Начать опрос</button>
         <button class="btn calc-btn" id="calc-next-btn">Далее</button>
         <button class="btn calc-btn" id="calc-finish-btn">Завершить опрос</button>
-        <div class="calc-block calc-finish">
+        <div class="calc-block calc-finish" id="calc-finish">
             <h4 class="headline">Результаты расчёта</h4>
             <table class="inner typeset">
                 <tr>
@@ -110,6 +121,8 @@
                     <td>1000 рублей</td>
                 </tr>
             </table>
+        <button class="btn calc-btn" data-action="openOrderForm">Оставить заявку</button>
+
         </div>
     </div>
 </section>
