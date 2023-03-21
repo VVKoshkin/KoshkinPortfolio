e001e1d121cf53543bca59e1fe29a602d6f859f1
VV.Koshkin <VV.Koshkin@e-soft.ru>
2023-03-21 15:25:12 +0300

    maintenance.php added

---
+++ maintenance.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Сайт временно выключен</title>
+</head>
+<body>
+    <h3>Сайт сейчас не работает</h3>
+    <p>Мы вернёмся через минутку</p>
+    
+</body>
+</html>
